91ff75d715ced2ff6a3b27dba18332ea007306c4
Matias Gajardo Castillo <mathi25gaj@gmail.com>
2025-08-26 12:17:24 -0400

    commit.commit

--- 4° SEMESTRE BACKEND/ACTIVIDAD_2_Multiples_tablas_DRF_(DOS)/api/views.py
+++ 4° SEMESTRE BACKEND/ACTIVIDAD_2_Multiples_tablas_DRF_(DOS)/api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    return HttpResponse("<h1>Bienvenido a la actividad Múltiples Tablas</h1><p>API funcionando correctamente.</p>")
--- 4° SEMESTRE BACKEND/ACTIVIDAD_2_Multiples_tablas_DRF_(DOS)/drf/urls.py
+++ 4° SEMESTRE BACKEND/ACTIVIDAD_2_Multiples_tablas_DRF_(DOS)/drf/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from api import views  # 👈 importa tu vista
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # aquí irán las rutas de la API (si las usas)
+    path('api/', include('api.urls')),
+    path('', views.home, name='home'),  # 👈 ruta raíz
 ]
